2fa3d121ed82b785d8fa34e2f8963319ab93e919
USER <mon001024@naver.com>
2025-07-31 10:56:05 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Counter from "./components/Counter";
+
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
-      App
+      <Counter />
     </div>
   );
 };
